61775e037f2c7547a38935ac026fcbe67f67c675
Miguel901826 <b2029lmadeguzman@pshs.edu.ph>
2026-01-20 22:32:32 +0800

    Finaler

--- Form.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false; // Prevents the form from submitting
-    }
-    // If validation passes, the form will submit to the action URL
-}
